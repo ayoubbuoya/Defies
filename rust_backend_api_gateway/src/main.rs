8e9e1ff3597e493472605f13787765983bd78f2b
Aziz__ <azizouboujneh@gmail.com>
2025-07-30 12:02:55 +0100

    updated the endpoint to get liquidity data of  pools in dragonswap and sailor

--- rust_backend_api_gateway/src/main.rs
+++ rust_backend_api_gateway/src/main.rs
@@ -1,5 +1,6 @@
 use actix_cors::Cors;
-use actix_web::{App, HttpServer, middleware::Logger, web};
+use actix_web::{App, HttpServer};
+use tracing_actix_web::TracingLogger;
 
 mod api;
 mod config;
@@ -12,24 +13,30 @@ use api::routes::init_routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
+    // This sets up a logger that will print all trace, debug, info, etc.
+    // messages to your console.
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_test_writer()
+        .init();
 
     HttpServer::new(|| {
         let cors = Cors::default()
-
-           // .allowed_origin("http://localhost:3000") // <-- Your frontend's URL
+            // In production, you should restrict this to your frontend's domain
+            // e.g., .allowed_origin("http://localhost:3000")
+            .allow_any_origin() 
             .allowed_methods(vec!["GET", "POST"])
-
             .allow_any_header()
-            .max_age(3600)
-        .supports_credentials();//for credentials and 
+            .max_age(3600);
 
         App::new()
             .wrap(cors)
-            .wrap(Logger::default())
+            // This TracingLogger replaces the old Logger::default()
+            // and integrates with the tracing system.
+            .wrap(TracingLogger::default()) 
             .configure(init_routes)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:8081")?
     .run()
     .await
 }
