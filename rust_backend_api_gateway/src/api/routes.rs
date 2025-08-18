b7457189e30e6f29ab53d8af51f19ac4448bc5dd
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2025-08-18 11:01:11 +0100

    fix

--- rust_backend_api_gateway/src/api/routes.rs
+++ rust_backend_api_gateway/src/api/routes.rs
@@ -1,9 +1,8 @@
 use crate::{
     api::handlers::{
-        add_position_handler, delete_position_handler, get_graph_data_handler, get_pools_handler,
-        get_positions_for_wallet, get_price_history_tool, get_token_pair_price_history,
-    add_chat,
-        get_token_symbol_handler, prompt_handler, verify_signature, get_price_history, 
+        add_chat, add_position_handler, delete_position_handler, get_chat, get_graph_data_handler,
+        get_pools_handler, get_positions_for_wallet, get_price_history_tool,
+        get_token_pair_price_history, get_token_symbol_handler, prompt_handler, verify_signature,
     },
     service,
 };
@@ -28,25 +27,13 @@ pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/agent").service(prompt_handler));
 
     cfg.service(
-
-    web::resource("/positions/{pb_key}")
-        .route(web::get().to(get_positions_for_wallet))
-);
-cfg.service(
-    web::resource("/positions")
-        .route(web::post().to(add_position_handler))
-);
-cfg.service(
-    web::resource("/positions/{pb_key}/{trans_id}")
-        .route(web::delete().to(delete_position_handler))
-);
-    cfg.service(
-        web::resource("/chat")
-            .route(web::put().to(add_chat))
+        web::resource("/positions/{pb_key}").route(web::get().to(get_positions_for_wallet)),
     );
+    cfg.service(web::resource("/positions").route(web::post().to(add_position_handler)));
     cfg.service(
-        web::resource("/chat/{public_key}")
-            .route(web::get().to(get_chat))
+        web::resource("/positions/{pb_key}/{trans_id}")
+            .route(web::delete().to(delete_position_handler)),
     );
+    cfg.service(web::resource("/chat").route(web::put().to(add_chat)));
+    cfg.service(web::resource("/chat/{public_key}").route(web::get().to(get_chat)));
 }
-
--- rust_backend_api_gateway/src/service/mod.rs
+++ rust_backend_api_gateway/src/service/mod.rs
@@ -1,9 +1,8 @@
 pub mod auth_service;
+pub mod chat_service;
 pub mod data_service;
 pub mod pool_service;
 pub mod position_service;
 pub mod price_history_tool_service;
-pub mod position_service;
-pub mod chat_service;
 pub mod prompt_pipeline_service;
 pub mod token_service;
